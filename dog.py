b974f9efc879542a09f161e4a1f4d85c6d161f16
beastsam <41358375+beastsam@users.noreply.github.com>
2018-10-18 16:17:54 +0800

    Create dog.py

---
+++ dog.py
@@ -0,0 +1,87 @@
+import random 
+import pandas as pd
+import numpy as np
+import tool
+
+from PIL import Image
+
+#图像大小为98*69
+#print(im.size,im.format,im.mode) 显示图片信息
+
+img = Image.open("dog.jpg","r")
+img_array=img.load()
+
+#随机选择四个中心点作为初始质心
+i1 = random.randint(0,97)     
+j1 = random.randint(0,68)   
+cent1 = img_array[i1,j1]
+
+i2 = random.randint(0,97)     
+j2 = random.randint(0,68)   
+cent2 = img_array[i2,j2]
+
+i3 = random.randint(0,97)     
+j3 = random.randint(0,68)   
+cent3 = img_array[i3,j3]
+
+i4 = random.randint(0,97)     
+j4 = random.randint(0,68)   
+cent4 = img_array[i4,j4]
+
+print(i1,j1,"\n",i2,j2,"\n",i3,j3,"\n",i4,j4)
+
+#聚类存储列表
+list1 = []
+list2 = []
+list3 = []
+list4 = []
+
+result = 1
+
+while result:
+	list1.clear
+	list2.clear
+	list3.clear
+	list4.clear
+	result = tool.pic(img,img_array,cent1,cent2,cent3,cent4,list1,list2,list3,list4,result)
+	
+imgnew1 = Image.new("RGB",(img.size[0],img.size[1]))
+imgnew2 = Image.new("RGB",(img.size[0],img.size[1]))
+imgnew3 = Image.new("RGB",(img.size[0],img.size[1]))
+imgnew4 = Image.new("RGB",(img.size[0],img.size[1]))
+
+#三个参数依次为R,G,B,A   R：红 G:绿 B:蓝 A:透明度
+#白色（225，255，255） 黑色（0，0，0）
+pixTuple = (255,255,255)
+for i in range(img.size[0]):
+	for j in range(img.size[1]):
+		if img_array[i,j] in list1:
+			imgnew1.putpixel((i,j),img_array[i,j])
+		else:
+			imgnew1.putpixel((i,j),pixTuple)
+
+		if img_array[i,j] in list2:
+			imgnew2.putpixel((i,j),img_array[i,j])
+		else:
+			imgnew2.putpixel((i,j),pixTuple)
+
+		if img_array[i,j] in list3:
+			imgnew3.putpixel((i,j),img_array[i,j])
+		else:
+			imgnew3.putpixel((i,j),pixTuple)
+
+		if img_array[i,j] in list4:
+			imgnew4.putpixel((i,j),img_array[i,j])
+		else:
+			imgnew4.putpixel((i,j),pixTuple)
+
+imgnew1.save('list1.jpg')
+imgnew2.save('list2.jpg')
+imgnew3.save('list3.jpg')
+imgnew4.save('list4.jpg')
+
+print("打印图片\n")
+
+
+
+
